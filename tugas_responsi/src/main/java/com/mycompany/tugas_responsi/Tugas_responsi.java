df35fccee2618cb28511bf439ae8766f1ba83ae3
IaMGeprekPanas <88416189+iqbalabdulmajid@users.noreply.github.com>
2023-05-29 19:12:34 +0700

    Add files via upload

---
+++ tugas_responsi/src/main/java/com/mycompany/tugas_responsi/Tugas_responsi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugas_responsi;
+
+/**
+ *
+ * @author Iqbal Abdul Majid
+ */
+public class Tugas_responsi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
